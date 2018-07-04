7e06f270278619a71089cc7c02ef5efd3908b093
coreygarza <coreygold888@gmail.com>
2018-07-04 21:54:44 +0800

    sparkline

--- dashboard/plugins/assets/js/init.js
+++ dashboard/plugins/assets/js/init.js
@@ -381,9 +381,24 @@ $('.switch-setting').each(function() {
 });
 /* Switchery end*/
  $(document).ready(function(){
-     $('#compact').DataTable( {
-         "ajax": 'https://autosector.com/get_coin_data.php?type=table'
-     } );
+     var table;
+     var currency="usd";
+ 	//function load_price_table(currency) {
+ 		//if(table)table.fnDestroy();
+        table=$('#compact').DataTable({
+            "ajax": 'https://autosector.com/get_coin_data.php?type=table&currency='+$("#currency1").val()
+        });
+    //}
+    // load_price_table("usd");
+ 	$("#currency1").change(function(){
+        var currency=$(this).val();
+        //table.ajax.reload();
+        table.ajax.url('https://autosector.com/get_coin_data.php?type=table&currency='+$("#currency1").val()).load();
+       // load_price_table(currency);
+	});
+     setInterval( function () {
+         table.ajax.url('https://autosector.com/get_coin_data.php?type=table&currency='+$("#currency1").val()).load();
+     }, 20000 );
      /*setInterval(function(){
      $.getJSON("https://autosector.com/get_coin_data.php?type=table", function(data) {
         var markets = data.data;

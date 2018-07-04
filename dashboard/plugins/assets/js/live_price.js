0d3d7f320d2ddd37168758190d48a013bb3f983a
coreygarza <coreygold888@gmail.com>
2018-07-05 07:37:47 +0800

    sparkline

--- dashboard/plugins/assets/js/live_price.js
+++ dashboard/plugins/assets/js/live_price.js
@@ -18,19 +18,4 @@ $(document).ready(function(){
     setInterval( function () {
         table.ajax.url('https://autosector.com/get_coin_data.php?type=table&currency='+$("#currency1").val()).load();
     }, 20000 );
-    /*setInterval(function(){
-     $.getJSON("https://autosector.com/get_coin_data.php?type=table", function(data) {
-     var markets = data.data;
-     for(var i=0;i<markets.length;i++){
-     var content=markets[i][0];
-     var id="#market"+$(content).text();
-     if($(id).closest("tr").find("td:nth-child(3)").length>0){
-     data=($(id).closest("tr").find("td:nth-child(3)").text());
-     if($(id).closest("tr").find("td:nth-child(3)").text()!=markets[i][2]) {
-     $(id).closest("tr").find("td:nth-child(3)").text(markets[i][2]);
-     }
-     }
-     }
-     });
-     }, 3000);*/
 });
--- dashboard/plugins/assets/js/monitoring.js
+++ dashboard/plugins/assets/js/monitoring.js
@@ -1,31 +1,46 @@
 $(document).ready(function(){
-    var cookie=document.cookie.split("=");
-    if(cookie[0]=="seed_key" && cookie[1]!="") {
-        var key = cookie[1].split(";");
-        if (key[0] != "") {
-            $("#my_content").hide();
-            $.ajax({
-                method: "GET",
-                url: "https://autosector.com/get_coin_data.php?type=login_data&key=" + key[0],
-                success: function (data) {
-                    var wrapped = $("<div>" + data + "</div>");
-                    wrapped.find(".navbar.navbar-inverse").remove();
-                    wrapped.find("footer").remove();
-                    html = wrapped.find(".container").html();
-                    $("#my_content").html(html);
-                    $("#my_content #banner, #my_content img,#my_content #seednr, #masternodes_table_length, #masternodes_table_filter, #masternodes_table_info, #masternodes_table_paginate").remove();
-                    $(".form-horizontal button").attr("type", "button");
-                    $("#my_content #masternodes_table").DataTable();
-                    if($("#my_content form").length<1){
-                        //document.cookie = "seed_key=";
-                        //$("#my_content a").attr("href","");
-                        $("#my_content a").attr("id","back_login");
+    get_login_data("https://autosector.com/get_coin_data.php?type=login_data");
+    function get_login_data(url) {
+        var cookie=document.cookie.split("=");
+        if(cookie[0]=="seed_key" && cookie[1]!="") {
+            var key = cookie[1].split(";");
+            if (key[0] != "") {
+                $("#my_content").fadeOut();
+                $.ajax({
+                    method: "GET",
+                    url: url+"&key=" + key[0],
+                    success: function (data) {
+                        var wrapped = $("<div>" + data + "</div>");
+                        wrapped.find(".navbar.navbar-inverse").remove();
+                        wrapped.find("footer").remove();
+                        html = wrapped.find(".container").html();
+                        $("#my_content").html(html);
+                        $("#my_content a").attr("href","");
+                        $("#my_content #banner, #my_content img,#my_content #seednr, #masternodes_table_length, #masternodes_table_filter, #masternodes_table_info, #masternodes_table_paginate").remove();
+                        $(".form-horizontal button").attr("type", "button");
+                        $("#my_content #masternodes_table").DataTable();
+                        if($("#my_content form").length<1){
+                            //document.cookie = "seed_key=";
+                            //$("#my_content a").attr("href","");
+                            $("#my_content a").attr("id","back_login");
+                        }
+                        $("#my_content").fadeIn();
                     }
-                    $("#my_content").show();
-                }
-            });
+                });
+            }
+
         }
     }
+    $(document).on("change","#change-currency",(function(e){
+        e.preventDefault();
+        //get_login_data("https://autosector.com/get_coin_data.php?type=login_data&currency="+$(this).val());
+    }));
+
+    $(document).on("click","#my_content form button",(function(e){
+        e.preventDefault();
+        //get_login_data("https://autosector.com/get_coin_data.php?type=login_data&address="+$("#my_content form #input").val());
+    }));
+
     $(document).on("click","#my_content div:first-child div:last-child>strong, #back_login",(function(e){
         e.preventDefault();
         document.cookie = "seed_key=";
